96c4c385582459588dd2c289e75d61a52e9b3c6c
Pam Tholan <pamelatholan@yahoo.com>
2019-07-22 21:34:23 -0400

    started styling survey page and completed friend.js file

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Billy',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/billy.jpg',
+		scores: [
+			'1',
+			'2',
+			'5',
+			'2',
+			'1',
+			'4',
+			'3',
+			'5',
+			'1',
+			'2'
+		]
+	},
+	{
+		name: 'Dustin',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/dustin.png',
+		scores: [
+			'4',
+			'4',
+			'1',
+			'5',
+			'4',
+			'3',
+			'4',
+			'2',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Eleven',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/eleven.jpg',
+		scores: [
+			'3',
+			'3',
+			'5',
+			'5',
+			'4',
+			'5',
+			'5',
+			'1',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Hopper',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/hopper.jpg',
+		scores: [
+			'3',
+			'2',
+			'2',
+			'3',
+			'1',
+			'5',
+			'5',
+			'4',
+			'1',
+			'2'
+		]
+	},
+	{
+		name: 'Lucas',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/lucas.jpg',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'3',
+			'5',
+			'3',
+			'4',
+			'3',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Max',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/max.jpg',
+		scores: [
+			'2',
+			'3',
+			'5',
+			'3',
+			'3',
+			'3',
+			'4',
+			'4',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Mike',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/mike.jpg',
+		scores: [
+			'4',
+			'4',
+			'3',
+			'4',
+			'3',
+			'3',
+			'3',
+			'2',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Robin',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/robin.png',
+		scores: [
+			'1',
+			'2',
+			'3',
+			'3',
+			'5',
+			'4',
+			'5',
+			'4',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Steve',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/steve.jpg',
+		scores: [
+			'3',
+			'2',
+			'3',
+			'3',
+			'4',
+			'2',
+			'3',
+			'3',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Will',
+		photo: 'https://github.com/pamelatholan/FriendFinder/blob/master/app/public/images/will.jpg',
+		scores: [
+			'5',
+			'5',
+			'1',
+			'5',
+			'4',
+			'2',
+			'4',
+			'1',
+			'3',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
